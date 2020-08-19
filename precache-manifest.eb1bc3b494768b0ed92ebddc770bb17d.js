eef45ef2cafc9c58e9d26de57ea6af5e896aacb2
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-08-19 15:10:45 +0200

    Updates

---
+++ precache-manifest.eb1bc3b494768b0ed92ebddc770bb17d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0500232304ecb1bdf38de13fded552a1",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "7ef5af2111e0102b7930",
+    "url": "/Robofriends/static/css/main.3e06c5c0.chunk.css"
+  },
+  {
+    "revision": "32bc208696a33c6c2c68",
+    "url": "/Robofriends/static/js/2.d72af8ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.d72af8ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef5af2111e0102b7930",
+    "url": "/Robofriends/static/js/main.f3f7cd40.chunk.js"
+  },
+  {
+    "revision": "e0d7379a9054147179a3",
+    "url": "/Robofriends/static/js/runtime-main.5a98dc6a.js"
+  }
+]);
